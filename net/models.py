189b187711a82a7fcbecf8a2406b17142cbfb924
Ceneve <boriskozlow@gmail.com>
2022-11-14 11:48:36 +0300

    Add api

--- net/models.py
+++ net/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 
 
-class Net(models.Model):
-    type = models.CharField(max_length=50)
-    parent = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True)
-
-
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
     product_model = models.CharField(max_length=100)
     product_launch_date = models.DateField(auto_now=False)
 
 
-class Factory(models.Model):
-    net_type = models.ForeignKey(Net, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+class Network(models.Model):
+    type = models.CharField(max_length=50)
+    parent = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True)
+    name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
@@ -24,6 +20,3 @@ class Factory(models.Model):
     debt = models.FloatField()
     products = models.ManyToManyField(Product)
     pub_date = models.DateTimeField('date published')
-
-
-
--- net/views.py
+++ net/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from .serializers import ProductSerializer, NetworkSerializer
+from .models import Product, Network
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def post(self, request):
+        product = request.data
+        serializer = ProductSerializer(data=product)
+        if serializer.is_valid(raise_exception=True):
+            product_saved = serializer.save()
+        return Response({"success": "Product '{}' created successfully".format(product_saved.product_name)})
+
+    def put(self, request, pk):
+        saved_product = get_object_or_404(Product.objects.all(), pk=pk)
+        data = request.data.get('products')
+        serializer = ProductSerializer(instance=saved_product, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            product_saved = serializer.save()
+        return Response({
+            "success": "Product '{}' updated successfully".format(product_saved.product_name)
+        })
+
+    def delete(self, request, pk):
+        article = get_object_or_404(Product.objects.all(), pk=pk)
+        article.delete()
+        return Response({
+            "message": "Product with id `{}` has been deleted.".format(pk)
+        }, status=204)
+
+
+class NetworkViewSet(viewsets.ModelViewSet):
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
+
+    def post(self, request):
+        network_elements = request.data
+        serializer = ProductSerializer(data=network_elements)
+        if serializer.is_valid(raise_exception=True):
+            network_elements_saved = serializer.save()
+        return Response({"success": "Product '{}' created successfully".format(network_elements_saved.product_name)})
+
+    def put(self, request, pk):
+        saved_network_element = get_object_or_404(Network.objects.all(), pk=pk)
+        data = request.data.get('networks')
+        serializer = NetworkSerializer(instance=saved_network_element, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            network_element_saved = serializer.save()
+        return Response({
+            "success": "Network_element '{}' updated successfully".format(network_element_saved.product_name)
+        })
+    #
+    # def delete(self, request, pk):
+    #     article = get_object_or_404(Product.objects.all(), pk=pk)
+    #     article.delete()
+    #     return Response({
+    #         "message": "Product with id `{}` has been deleted.".format(pk)
+    #     }, status=204)
